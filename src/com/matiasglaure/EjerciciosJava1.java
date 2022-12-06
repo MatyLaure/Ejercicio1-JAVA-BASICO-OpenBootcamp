6e9f6c37a6895d85fcd43fd5d611d5a65579405f
Matias Andres Garcia Laure <garcialaurema@gmail.com>
2022-12-06 20:05:30 -0300

    JAVA BASICO 1

---
+++ src/com/matiasglaure/EjerciciosJava1.java
@@ -0,0 +1,29 @@
+package com.matiasglaure;
+
+public class EjerciciosJava1 {
+
+    public static void main(String[] args) {
+        int numeroEntero = 25;
+        System.out.println(numeroEntero);
+        double numeroDecimal = 2.149d;
+        System.out.println(numeroDecimal);
+        long numero = 123456789;
+        System.out.println(numero);
+        boolean siOno = false;
+            if (siOno) {
+                System.out.println("es mentira");
+            } else System.out.println("es verdad");
+        System.out.println(siOno);
+        String escritura = "Mi Primer entrega de JAVA BASICO ";
+        System.out.println(escritura);
+    }
+
+    class datos {
+        int numeroEntero ;
+        double numeroDecimal;
+        long numero;
+        boolean siOno;
+        String escritura;
+
+    }
+}
